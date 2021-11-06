86a832a002704152c1f44386664bdfabafafbaa9
Wellington Nascimento <wnsferreira@gmail.com>
2021-11-06 19:59:45 -0300

    Home, navigation

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,22 @@
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home></Home>
+                </Route>
+
+                <Route path="/dashboard">
+                    <Dashboard></Dashboard>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
